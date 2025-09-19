3e98c9e0f22382750b395000468795626a776c46
Vishwesvaran <vishwesvaranm25@gmail.com>
2025-09-19 09:49:57 +0530

    Add sign up Authentication using Appwrite

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,12 @@
+import Authform from '@/components/Authform'
 import React from 'react'
 
-const SignIn = () => {
+const SignIn =  () => {
+  
   return (
-    <div>SignIn</div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <Authform type="sign-in" />
+    </section>
   )
 }
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,15 @@
+import Authform from '@/components/Authform'
+
 import React from 'react'
 
-const SignUp = () => {
+
+const SignUp = async () => {
+  
   return (
-    <div>SignUp</div>
+    <section className='flex-center size-full max-sm:px-6'>
+      <Authform type='sign-up' />
+
+    </section>
   )
 }
 
