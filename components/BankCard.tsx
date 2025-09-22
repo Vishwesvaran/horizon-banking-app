ad0cafd1cc63f16dc32831620f630607122f8ba1
Vishwesvaran <vishwesvaranm25@gmail.com>
2025-09-22 10:52:36 +0530

    add transaction page and my bank page

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { clsx } from 'clsx';
+import Copy from './Copy';
 
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
     return (
         <div className='flex flex-col'>
-            <Link href={'/'} className='bank-card'>
+            <Link href={`transaction-history/?id=${account.appwriteItemId}`} className='bank-card'>
                 <div className='bank-card_content'>
                     <div>
                         <h1 className='text-16 font-semibold text-white'>{userName}</h1>
@@ -20,7 +21,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                             <h2 className='text-12 font-semibold text-white'>●● / ●●</h2>
                         </div>
                         <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                            ●●●● ●●●● ●●●● <span className='text-16'>1234</span>
+                            ●●●● ●●●● ●●●● <span className='text-16'>{account?.mask}</span>
                         </p>
 
                     </article>
@@ -48,6 +49,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                     className='absolute top-0 left-0'
                 />
             </Link>
+            {showBalance && <Copy title={account.sharableId}/>}
         </div>
     )
 }
--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -3,6 +3,7 @@ import { Button } from './ui/button'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 import { useRouter } from 'next/navigation'
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
+import Image from 'next/image'
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     const router = useRouter()
@@ -17,7 +18,7 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
         getLinkToken()
     }, [user])
 
-    
+
 
     const onSuccess = useCallback<PlaidLinkOnSuccess>(async (plubic_token: string) => {
         await exchangePublicToken({
@@ -43,12 +44,24 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
                     Connect Bank
                 </Button>
             ) : variant === 'ghost' ? (
-                <Button>
-                    Connect Bank
+                <Button className='plaidlink-ghost' variant='ghost' onClick={() => open()} >
+                    <Image
+                        src={"/icons/connect-bank.svg"}
+                        alt='connect bank'
+                        width={24}
+                        height={24}
+                    />
+                    <p className='text-[16px] hidden font-semibold text-black-2'>Connect Bank</p>
                 </Button>
             ) : (
-                <Button>
-                    Connect Bank
+                <Button className='plaidlink-default' variant='default' onClick={() => open()} >
+                    <Image
+                        src={"/icons/connect-bank.svg"}
+                        alt='connect bank'
+                        width={24}
+                        height={24}
+                    />
+                    <p className='text-[16px] font-semibold text-black-2'>Connect Bank</p>
                 </Button>
             )}
         </>
